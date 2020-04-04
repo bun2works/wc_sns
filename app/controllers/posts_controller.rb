6d9896f0e9e6a8a83821918d06a03c60cc98f5fd
bun2works <bun2works@gmail.com>
2020-04-04 14:29:46 +0900

    add new create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(title)
+    params.require(:post).permit(body)
+  end
+
 end
