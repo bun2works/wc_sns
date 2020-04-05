5edce3d47abcc29668e02f2e589a08fa4870ee80
bun2works <bun2works@gmail.com>
2020-04-06 03:10:08 +0900

    add Devise

---
+++ app/models/team.rb
@@ -0,0 +1,19 @@
+class Team < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one_attached :team_image
+  validates :name, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :representative, presence: true, length: {maximum: 20}
+  validates :homepage, length: {maximum: 100}, uniqueness: true
+  validates :location_and_day, presence: true, length: {maximum: 50}
+  validates :introduction, presence: true, length: {maximum: 100}
+
+  def team_image_check
+    unless team_image.content_type.in?(%(image/jpeg image/png))
+      errors.add(:team_image, 'にはjpegまたはpngファイルを添付してください。')
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_053330) do
+ActiveRecord::Schema.define(version: 2020_04_05_104150) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,23 @@ ActiveRecord::Schema.define(version: 2020_04_04_053330) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "representative", null: false
+    t.string "homepage"
+    t.string "location_and_day", null: false
+    t.text "introduction", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_teams_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_teams_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
