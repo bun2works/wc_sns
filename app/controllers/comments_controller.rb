d81499d6bb864d142777fdcc23260be143d652fe
bun2works <bun2works@gmail.com>
2020-04-06 19:33:51 +0900

    add gem kaminari

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to posts_path, notice: "レスポンスを投稿しました。"
     else
-      @posts = Post.order(updated_at: :desc)
+      @posts = Post.order(updated_at: :desc).page(params[:page]).per(10)
       render template: "posts/index"
     end
   end
