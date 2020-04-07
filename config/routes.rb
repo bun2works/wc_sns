70fa03295a9983a71e2f1db2d85d9180fe76ce9d
bun2works <bun2works@gmail.com>
2020-04-07 19:56:50 +0900

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :teams
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admin_users, controllers: {
+    sessions:      'admin_users/sessions',
+    passwords:     'admin_users/passwords',
+    registrations: 'admin_users/registrations'
+  }
+  devise_for :teams, controllers: {
+    sessions:      'teams/sessions',
+    passwords:     'teams/passwords',
+    registrations: 'teams/registrations'
+  }  
   resources :posts
   resources :comments, only: [:create, :destroy]
   resources :teams, only: :show
